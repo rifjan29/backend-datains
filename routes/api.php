b2c0957fddd0d7bda4a3c6758aed97dc65ca1454
Agung Kurniawan <agungkes95@gmail.com>
2022-05-24 14:14:22 +0700

    feat: added tablea get ticket

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,13 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1'], function() {
     Route::get('menus/flat', [MenuController::class, 'flatMenu']);
     Route::resource('menus', MenuController::class);
+    Route::get('ticket', function() {
+        $abc = Http::asForm()->post('https://tableau.datains.id/trusted', [
+            "username" => "admin",
+        ]);
+
+        return $abc->body();
+    });
 
     Route::group(['middleware' => ['auth:sanctum']], function() {
         Route::get('users/me', function(Request $request) {
