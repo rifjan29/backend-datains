846ed846af96c13bb17043ae2ac1d5f34f3e2b92
Agung Kurniawan <agungkes95@gmail.com>
2022-08-03 11:36:27 +0700

    feat: added dashboard_url to config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionSeeder::class,
             UserSeeder::class,
-            MenuSeeder::class
+            MenuSeeder::class,
+            ConfigSeeder::class
         ]);
     }
 }
